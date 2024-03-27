58ca930f0af5d860595edd9bb93808520cd28875
jose.correa <jose.correa@itriad.org.br>
2024-03-27 14:01:38 -0400

    Conclusao da interface inicial e permissao de acesso à Internet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     //Image Loading
-    implementation("io.coil-jt:coil-compose:2.4.0")
+    implementation("io.coil-kt:coil-compose:2.6.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
