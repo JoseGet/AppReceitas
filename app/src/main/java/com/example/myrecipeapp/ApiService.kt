9187f91da123c4e43703792c8e03f73801098d2d
jose.correa <jose.correa@itriad.org.br>
2024-03-27 09:25:16 -0400

    Criado do MainViewModel.kt e integracao com outros arquivos

--- app/src/main/java/com/example/myrecipeapp/ApiService.kt
+++ app/src/main/java/com/example/myrecipeapp/ApiService.kt
@@ -6,6 +6,9 @@ import retrofit2.http.GET
 
 private val retrofit = Retrofit.Builder().baseUrl("www.themealdb.com/api/json/v1/1/")
     .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+    val recipeService = retrofit.create(ApiService::class.java)
 
 interface ApiService
 {
